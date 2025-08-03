05f6d8fe80b2a4ffd21cd38d86d7677c0aab5321
DilmiAmanda <amandhadilmi@gmail.com>
2025-08-03 21:23:59 +0530

    create functions getproduct and create product

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,5 +2,36 @@ import product from "../models/product.js";
 
 export function createProduct(req, res) {
 
-    
-}
+   if(!isAdmin(req)) {
+       res.json({
+        message:"Please login as administrator to add products"
+       })
+
+
+   }
+     
+
+        const newProductData = req.body; // Get product data from the request body
+
+        const newProduct = new product(newProductData); // Create a new product instance with the request body
+        newProduct.save() // Save the new product to the database
+            .then(() => {
+                res.json({ message: 'Product created successfully' }); // Respond with success message
+            })
+            .catch((err) => {
+                res.json({ 
+                    message: err
+                }); // Handle errors
+            });
+    }
+
+    export function getProducts(req, res) {
+        product.find({}).then((products) => {
+            res.json(products); // Respond with the list of products
+        }) 
+    }
+
+
+
+
+
