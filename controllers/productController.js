95cfe635fcd361c2b4b5b9ed96123443bfa84afe
DilmiAmanda <amandhadilmi@gmail.com>
2025-08-03 20:07:39 +0530

    create a new function in the user.js to check the admin

---
+++ controllers/productController.js
@@ -0,0 +1,6 @@
+import product from "../models/product.js";    
+
+export function createProduct(req, res) {
+
+    
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -93,7 +93,31 @@ export function createUser(req, res) {
     }
 
 
+    export function isAdmin(req){
+
+        if(req.user == null){
+            return false; // If no user is logged in, return false
+        }
+ 
+        if(req.user.type!="customer"){
+            return false; // If the user type is not 'customer', return true (indicating admin)
+        }
+
+        return true; // If the user type is 'customer', return true (indicating admin)
+    }
 
 
-//  "password": "hashedpassword123", 
+
+
+//  "password": "hashedpassword123", admin
 //  "email": "exampleuser@gmail.com",
+
+
+//  "email": "exampleuser5@gmail.com",customer
+//  "password": "hashedpassword123", 
+
+
+/*"email": "johnsmith2025@example.com",
+  "firstname": "John",
+  "lastname": "Smith",
+  "password": "StrongPass!789",*/
