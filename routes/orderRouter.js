16ef2d2506574ee5c8e2ecf66fe8ad3c361ec91a
DilmiAmanda <amandhadilmi@gmail.com>
2025-08-06 19:44:45 +0530

    changes

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,9 +1,9 @@
- import express from 'express';
+import express from 'express';
 import { createOrder, getOrders } from '../controllers/orderController.js'; // ✅ IMPORT getOrders
 
 const orderRouter = express.Router();
 
 orderRouter.post('/', createOrder);
-orderRouter.get('/', getOrders); // ✅ Now this will work
+orderRouter.get('/', getOrders); // 
 
 export default orderRouter;
