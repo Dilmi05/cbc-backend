30d7c22e6fc875d1dafe5780e71de74f40a02793
DilmiAmanda <amandhadilmi@gmail.com>
2025-08-10 14:38:05 +0530

    install cors

--- index.js
+++ index.js
@@ -1,64 +1,54 @@
 // index.js
 import express from 'express'; // Import Express
-
-import bodyParser from 'body-parser'; 
-
-
-
-
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose'; // Import Mongoose
- const app = express();  
-
+const app = express();
 import userRouter from './routes/userRouter.js'; // Import the user routes
-
-
 import orderRouter from './routes/orderRouter.js'; // Import the order routes
-
 import dotenv from "dotenv"; // Import dotenv for environment variables
 dotenv.config() // Load environment variables from .env file
-
 import jwt from 'jsonwebtoken'; // Import jsonwebtoken for token generation
-const mongodbUrl= process.env.MONGO_DB_URI// Create Express app
-
-
+const mongodbUrl = process.env.MONGO_DB_URI// Create Express app
 import productRouter from './routes/productRouter.js'; // Import the product routes
+import cors from
 
 
 app.use(bodyParser.json()); // Middleware to parse JSON bodies
 
-app.use((req,res,next) => {
- 
+app.use((req, res, next) => {
 
-  const token =req.header('authorization')?.replace("Bearer","") // Log the authorization header
+const token = req.header('authorization')?.replace("Bearer", "") // Log the authorization header
 
-   
 
-  if(token!= null){
-    jwt.verify(token,process.env.SECRET,(err,decoded)=>{
-      if(!err){
+
+  if (token != null) {
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (!err) {
 
         console.log(decoded); // Log the decoded token
         req.user = decoded; // Attach the decoded user information to the request object
-        
+
       }
-      })
+    })
 
   }
-   next(); // Call the next middleware
+  next(); // Call the next middleware
 }
 )
 
-mongoose.connect(mongodbUrl,{})
+mongoose.connect(mongodbUrl, {})
+
+app.use(cors()); // Enable CORS for all routes
 
 const connection = mongoose.connection; // Create a connection to MongoDB
-connection.once("open",() => {
-    console.log("MongoDB connection established successfully");
+connection.once("open", () => {
+  console.log("MongoDB connection established successfully");
 });
 
 const PORT = 5000;
 
- 
- app.use('/api/users', userRouter); // Use the user router for /users routes
+
+app.use('/api/users', userRouter); // Use the user router for /users routes
 // Start the server
 
 app.use('/api/products', productRouter); // Use the product router for /products routes
@@ -68,4 +58,3 @@ app.use('/api/orders', orderRouter); // Use the order router for /orders
 app.listen(PORT, () => {
   console.log("Server is running at http://localhost:${PORT}");
 });
- 
